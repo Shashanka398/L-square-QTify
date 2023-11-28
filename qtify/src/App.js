0f3a0c5192e59084134c238be8bd6e6b47077bbb
shashank398@gmail.com <you@example.com>
2023-11-28 23:11:00 +0530

    Added hero section

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import SearchBar from "./components/SearchBar/SearchBar";
 import NavBar  from "./components/NavBar/NavBar";
+import HeroSection from "./components/HeroSection/HeroSection";
 import "./index.css"
 function App() {
 	return (
 		<div className="App">
 			<NavBar/>
+			<HeroSection/>
 		</div>
 	);
 }
