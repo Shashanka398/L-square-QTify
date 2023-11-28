90071f9157f87c98e4078fde529efe154e36f15c
shashank398@gmail.com <you@example.com>
2023-11-28 22:10:47 +0530

    Navbar updated

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -2,15 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import SearchBar from "./components/SearchBar/SearchBar";
 import NavBar  from "./components/NavBar/NavBar";
+import "./index.css"
 function App() {
 	return (
 		<div className="App">
 			<NavBar/>
-			
-			
-
-			
-			
 		</div>
 	);
 }
