3a453bafedbb5b8863e4d70388e4246127516fe6
shashank398@gmail.com <you@example.com>
2023-11-28 23:02:22 +0530

    updated search

--- qtify/src/components/SearchBar/SearchBar.jsx
+++ qtify/src/components/SearchBar/SearchBar.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import {ReactComponent as SearchIcon} from "../../assets/Search icon.svg"
 import styles from "./SearchBar.module.css"
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const {placeholder}=props
   return (
     <form className={styles.wrapper}>
-        <input className={styles.search} placeholder="search" required/>
+        <input className={styles.search} placeholder={placeholder} required/>
         <button className={styles.searchButton} type='search'  >
             <SearchIcon/>
         </button>
