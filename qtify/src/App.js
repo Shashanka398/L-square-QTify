7f3dc09e634e513f09f7020135f88c3f6cbb22c6
shashank398@gmail.com <you@example.com>
2023-11-29 00:05:36 +0530

    updated cards and hero

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,16 +1,36 @@
-import React from "react";
+
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import SearchBar from "./components/SearchBar/SearchBar";
-import NavBar  from "./components/NavBar/NavBar";
+import NavBar from "./components/NavBar/NavBar";
 import HeroSection from "./components/HeroSection/HeroSection";
-import "./index.css"
+import Section from "./components/Section/Section";
+import "./index.css";
+
 function App() {
-	return (
-		<div className="App">
-			<NavBar/>
-			<HeroSection/>
-		</div>
-	);
+  const [album, setAlbum] = useState([]);
+
+
+  useEffect(() => {
+    const getAlbum = async () => {
+      try {
+        const res = await fetch("https://qtify-backend-labs.crio.do/albums/top");
+        const data = await res.json();
+        setAlbum(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAlbum();
+  }, []); 
+  return (
+    <div className="App">
+      <NavBar />
+      <HeroSection />
+      <Section title="Top Albums" data={album} type="album" />
+    </div>
+  );
 }
+
 
 export default App;
