01ae06b45b2a354f959a5f71d1208ad83107d68a
shashank398@gmail.com <you@example.com>
2023-11-30 21:38:08 +0530

    updated

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,51 +1,16 @@
 
 import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-import SearchBar from "./components/SearchBar/SearchBar";
-import NavBar from "./components/NavBar/NavBar";
-import HeroSection from "./components/HeroSection/HeroSection";
-import Section from "./components/Section/Section";
+import HomePage from "./pages/HomePage";
 import "./index.css";
 import styles from "./App.module.css"
 
 
 function App() {
-  const [topAlbum, setTopAlbum] = useState([]);
-  const [newAlbum, setNewAlbum] = useState([]);
 
-
-  useEffect(() => {
-    const getAlbum = async () => {
-      try {
-        const res = await fetch("https://qtify-backend-labs.crio.do/albums/top");
-        const data = await res.json();
-        setTopAlbum(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getAlbum();
-  }, []); 
-  useEffect(() => {
-    const getAlbum = async () => {
-      try {
-        const res = await fetch("https://qtify-backend-labs.crio.do/albums/new");
-        const data = await res.json();
-		setNewAlbum(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getAlbum();
-  }, []); 
   return (
     <div className="App">
-      <NavBar />
-      <HeroSection />
-	  <div className={styles.sectionWrapper}>
-      <Section title="Top Albums" data={topAlbum} type="album" />
-	  <Section title="New Albums" data={newAlbum} type="album" />
-	 </div>
+      <HomePage/>
     </div>
   );
 }
